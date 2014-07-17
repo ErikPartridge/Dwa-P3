fc6622a664de06e27e05b71a105f4b2cd057fa5b
Erik Malmstrom-Partridge <erazor101@gmail.com>
2014-07-16 22:36:38 -0400

    HTML validation

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -1,12 +1,13 @@
 <!Doctype html>
 <html>
-    <!--- This extends the master template --->
+    <!--- This extends the master template -->
     <head>
 
         <meta charset="UTF-8">
         {{ HTML::style("/css/main.css"); }}
         <meta name="author" content="Erik Malmstrom-Partridge">
         <title>@yield('title')</title>
+        @yield('style')
     </head>
     <body>
         @yield('content')
--- app/views/lipsum.blade.php
+++ app/views/lipsum.blade.php
@@ -3,14 +3,15 @@
 @section('title')
 Lorem Ipsum Text
 @stop
-
+@section('style')
+<style> body{padding-right: 0%; padding-left: 0%;}</style>
+@stop
 
 @section('content')
 
 <?php
 use \Badcow\LoremIpsum as Lipsum;
 // Make it take whole screen, because there is so much text
-$style = "<style> body{padding-right: 0%; padding-left: 0%;}</style>";
 
 $gen = new Lipsum\Generator();
 
@@ -18,7 +19,7 @@ $text = $gen->getParagraphs($paragraphs);
 //put text in paragraph form
 $string = "<div class=\"lipsum\">".implode("<p>", $text)."</div>";
 //echo out the text
-echo $string.$style;
+echo $string;
 
 ?>
 
